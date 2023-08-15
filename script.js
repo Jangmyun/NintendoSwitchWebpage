6859aa3155fcd9505985a4d5ceb780f601a0822a
Jangmyun <cndlsrb2739@gmail.com>
2023-08-16 01:55:13 +0900

    순차적 이미지 표시에 사용한 코드에 주석 추가

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-var kbImgs = document.querySelectorAll(".kb div");
+var kbImgs = document.querySelectorAll(".kb div");  //커비 이미지박스 노드리스트
 var i = 1;
-
 console.log(kbImgs);
-
-
-// 페이지 로딩 완료시 순차적으로 별의 커비 이미지가 보이도록 함
+//순차적으로 별의 커비 이미지가 보이도록 함
+//노드 리스트는 엄밀히 배열이 아니므로 forEach.call 메서드를 사용하면 Array의 메서드를 빌려사용할 수 있다.
 Array.prototype.forEach.call(kbImgs, function(kbImg){
     setTimeout(function(){ kbImg.classList.add("visible")} , 300*i);
     if(i >= 7 ){        //7번째 사진 (별의커비 로고)부터 다른 사진이 뜨는 속도의 2배 느리게 보이도록 함
