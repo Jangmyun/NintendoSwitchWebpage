47a1de1a009dba5072ddac192fdbbb20dd165229
Jangmyun <cndlsrb2739@gmail.com>
2023-08-17 11:08:50 +0900

    JS 파일 폴더 따로 만들어서 분리

--- JS/game_title_display.js
+++ JS/game_title_display.js
@@ -5,9 +5,7 @@ console.log(kbImgs);
 //노드 리스트는 엄밀히 배열이 아니므로 forEach.call 메서드를 사용하면 Array의 메서드를 빌려사용할 수 있다.
 Array.prototype.forEach.call(kbImgs, function(kbImg){
     setTimeout(function(){ kbImg.classList.add("visible")} , 300*i);
-    if(i >= 7 ){        //7번째 사진 (별의커비 로고)부터 다른 사진이 뜨는 속도의 2배 느리게 보이도록 함
-        i += 2;
-    } else {
-        i++;
-    }
+    //7번째 사진 (별의커비 로고)부터 다른 사진이 뜨는 속도의 2배 느리게 보이도록 함
+    if(i >= 7 ) i += 2;        
+    else i++;
 })
